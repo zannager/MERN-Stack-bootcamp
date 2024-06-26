444d6efddd529e0cfcb17dd9fecfc197f5547061
zannager <gzraenga@gmail.com>
2024-06-27 02:49:37 +0800

    window object, dom and local storage

--- modernjs/index.js
+++ modernjs/index.js
@@ -1,1 +1,8 @@
-console.log('learning modern js')
+// let heading = document.querySelector('h1')
+// heading.innerText = "PHP"
+
+console.log(document.URL)
+
+// window.alert('learning js')
+
+window.localStorage.removeItem('test')
