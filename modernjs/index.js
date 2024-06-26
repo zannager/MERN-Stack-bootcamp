dfe96b59c5f887ed2f7e4b36c36d9536ec68a10d
zannager <gzraenga@gmail.com>
2024-06-27 03:06:56 +0800

    var, let, const

--- modernjs/index.js
+++ modernjs/index.js
@@ -1,8 +1,30 @@
+// window object, DOM, local storage
 // let heading = document.querySelector('h1')
 // heading.innerText = "PHP"
-
-console.log(document.URL)
-
+// console.log(document.URL)
 // window.alert('learning js')
+// window.localStorage.removeItem('test')
 
-window.localStorage.removeItem('test')
+
+// var let cons
+//var
+// var n = 'greg'
+// n = 'red'
+// alert(n)
+
+//let
+// let n1 = 'greg'
+// n1 = 'green'
+// alert(n1)
+
+// if(true) {
+//     var n2 = 'zen'
+//     let n3 = 'desk'
+//     alert(n3)
+// }
+// alert(n2)
+
+//const
+const n4 = 'blue'
+n4 = 'black'
+alert('n4')
