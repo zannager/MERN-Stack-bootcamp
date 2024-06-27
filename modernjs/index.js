784dd32d97464e571c7794e8d3cfa7ab33889b12
zannager <gzraenga@gmail.com>
2024-06-28 01:43:59 +0800

    object destructuring

--- modernjs/index.js
+++ modernjs/index.js
@@ -1,16 +1,51 @@
-//this keywoard
+//object destructuring
 
-// function sayHi() {
-//   console.log(this);
-// }
-
-// sayHi();
-
-let australia = {
-  cities: ["sydney", "melbourne", "adelaide"],
-  printWithDash: function () {
-    alert(this.cities.join(" - "));
-  },
+//old
+let thingsToDo = {
+  morning: "exercise",
+  afternoon: "workd",
+  evening: "code",
+  night: ["sleep", "dream"],
 };
 
-australia.printWithDash();
+console.log(thingsToDo.morning);
+
+//new
+let { morning, afternoon, evening, night } = thingsToDo;
+
+console.log(night);
+
+//destructuring function arguments
+//passing an object as function arguments
+
+//old
+// const uniStudents = (student) => {
+//   console.log(`${student.fname} is from unversity of baguio`);
+// };
+
+// uniStudents({
+//   fname: "greg",
+//   lname: "boy",
+// });
+
+//new
+// const uniStudents = (student) => {
+//   const { fname, lname } = student;
+//   console.log(`${fname} ${lname} is from unversity of baguio`);
+// };
+
+// uniStudents({
+//   fname: "greg",
+//   lname: "yaman",
+// });
+
+//or
+const uniStudents = ({ fname, lname }) => {
+  // const { fname, lname } = student;
+  console.log(`${fname} ${lname} is from unversity of baguio`);
+};
+
+uniStudents({
+  fname: "greg",
+  lname: "yaman",
+});
