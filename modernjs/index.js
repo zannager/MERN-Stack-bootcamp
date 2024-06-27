6fb2f3191a54df48c9de6b99e6a295d08bc52197
zannager <gzraenga@gmail.com>
2024-06-28 02:36:08 +0800

    destructuring arrays

--- modernjs/index.js
+++ modernjs/index.js
@@ -1,51 +1,12 @@
-//object destructuring
+// destructuring arrays
+const item = ["one", "two", "three"];
+console.log(item);
 
-//old
-let thingsToDo = {
-  morning: "exercise",
-  afternoon: "workd",
-  evening: "code",
-  night: ["sleep", "dream"],
-};
+const [arr] = ["one", "two", "three"];
+console.log(arr);
 
-console.log(thingsToDo.morning);
+const [, arr1] = ["one", "two", "three"];
+console.log(arr1);
 
-//new
-let { morning, afternoon, evening, night } = thingsToDo;
-
-console.log(night);
-
-//destructuring function arguments
-//passing an object as function arguments
-
-//old
-// const uniStudents = (student) => {
-//   console.log(`${student.fname} is from unversity of baguio`);
-// };
-
-// uniStudents({
-//   fname: "greg",
-//   lname: "boy",
-// });
-
-//new
-// const uniStudents = (student) => {
-//   const { fname, lname } = student;
-//   console.log(`${fname} ${lname} is from unversity of baguio`);
-// };
-
-// uniStudents({
-//   fname: "greg",
-//   lname: "yaman",
-// });
-
-//or
-const uniStudents = ({ fname, lname }) => {
-  // const { fname, lname } = student;
-  console.log(`${fname} ${lname} is from unversity of baguio`);
-};
-
-uniStudents({
-  fname: "greg",
-  lname: "yaman",
-});
+const [, , arr2] = ["one", "two", "three"];
+console.log(arr2);
