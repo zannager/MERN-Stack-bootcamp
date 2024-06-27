7b7745a9e3e63fd0675a8afcd262a67c98d60bae
zannager <gzraenga@gmail.com>
2024-06-27 11:43:13 +0800

    arrow function and return keyword

--- modernjs/index.js
+++ modernjs/index.js
@@ -1,5 +1,19 @@
-//function and default arguments
-function welcome(user = "greg", message = "good night") {
-  alert(`hello ${user}, ${message}`);
-}
-welcome();
+// arrow function and return keyword
+// function greeting(message) {
+//   alert(`${message} everyone!`);
+// }
+
+// const greeting = (message) => alert(`${message} everyone!`);
+
+// greeting("hello");
+
+const createBlog = (title, body) => {
+  if (!title) {
+    throw new Error("a title is needed");
+  }
+  if (!body) {
+    throw new Error("body can not be empty");
+  }
+  return `${title} - ${body}`;
+};
+alert(createBlog("a blog title", "this is my first blog"));
