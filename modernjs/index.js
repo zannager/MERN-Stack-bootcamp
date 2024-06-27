125db5cec4f9d377b8ce3dc5c98483fb5bb4af55
zannager <gzraenga@gmail.com>
2024-06-28 01:25:27 +0800

    this keyword

--- modernjs/index.js
+++ modernjs/index.js
@@ -1,19 +1,16 @@
-// arrow function and return keyword
-// function greeting(message) {
-//   alert(`${message} everyone!`);
+//this keywoard
+
+// function sayHi() {
+//   console.log(this);
 // }
 
-// const greeting = (message) => alert(`${message} everyone!`);
+// sayHi();
 
-// greeting("hello");
-
-const createBlog = (title, body) => {
-  if (!title) {
-    throw new Error("a title is needed");
-  }
-  if (!body) {
-    throw new Error("body can not be empty");
-  }
-  return `${title} - ${body}`;
+let australia = {
+  cities: ["sydney", "melbourne", "adelaide"],
+  printWithDash: function () {
+    alert(this.cities.join(" - "));
+  },
 };
-alert(createBlog("a blog title", "this is my first blog"));
+
+australia.printWithDash();
