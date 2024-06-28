70069e916b38497ca803e2656c35673cf7f2f1cf
zannager <gzraenga@gmail.com>
2024-06-29 01:20:56 +0800

    classes

--- modernjs/index.js
+++ modernjs/index.js
@@ -1,19 +1,17 @@
-// function and prototype
+// classes
 
-function Holiday(destination, days) {
-  this.destination = destination;
-  this.days = days;
+class Holiday {
+  //constructor
+  constructor(destination, days) {
+    this.destination = destination;
+    this.days = days;
+  }
+
+  info() {
+    console.log(`${this.destination} will take ${this.days} days.`);
+  }
 }
 
-Holiday.prototype.info = function () {
-  console.log(`${this.destination} | ${this.days}`);
-};
-
-const nepal = new Holiday("nepal", 30);
-console.log(nepal.destination, nepal.days);
-
-const australia = new Holiday("Australia", 7);
-console.log(australia.destination, australia.days);
-
-nepal.info(); // Output: "nepal | 30"
-australia.info(); // Output: "Australia | 7"
+const trip = new Holiday("nepal", 30);
+console.log(trip.info());
+console.log(Holiday.prototype);
