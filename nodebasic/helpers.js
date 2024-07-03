c30d02c81713e5461da99e359f475bf9bb9ba569
zannager <gzraenga@gmail.com>
2024-07-04 02:37:04 +0800

    require and export

---
+++ nodebasic/helpers.js
@@ -0,0 +1,12 @@
+// function sum(a, b) {
+//   return a + b;
+// }
+
+// module.exports = {
+//   sum,
+// };
+
+//or
+exports.sum = (a, b) => {
+  return a + b;
+};
--- nodebasic/index.js
+++ nodebasic/index.js
@@ -1,1 +1,10 @@
-console.log("nodemon is watching...");
+// const helpers = require("./helpers");
+
+// const total = helpers.sum(10, 200);
+// console.log(total);
+
+//or
+const { sum } = require("./helpers");
+
+const total = sum(10, 100);
+console.log(total);
