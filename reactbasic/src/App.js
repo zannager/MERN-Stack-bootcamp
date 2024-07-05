b14add283dd4906e3f7fd6f6e2f94ccc1a46ebbb
zannager <gzraenga@gmail.com>
2024-07-06 02:45:33 +0800

    creating a form

--- reactbasic/src/App.js
+++ reactbasic/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1 className='display-4 text-center'>Learning react</h1>
+      <br />
+
+      <div className='row'>
+        <div className='col-md-6 offset-md-r'>
+          <form>
+            <div className='mb-3'>
+              <input className='form-control' type='text' />
+            </div>
+            <div className='mb-3'>
+              <input className='form-control' type='password' />
+            </div>
+            <button type='submit' className='btn btn-primary'>
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
