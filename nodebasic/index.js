33f7f5cdfdd829a2488da2309376420f0b9e5cfc
zannager <gzraenga@gmail.com>
2024-07-09 03:19:12 +0800

    Handling CORS error

--- nodebasic/index.js
+++ nodebasic/index.js
@@ -1,16 +1,25 @@
 //using expressjs
 
 const express = require("express");
+const cors = require("cors");
 const app = express();
+
+app.use(cors());
 
 //get post put delete
 app.get("/api", function (req, res) {
   //   res.send("hello from node api");
   res.json({
-    user: {
-      name: "greg",
-      age: 30,
-    },
+    users: [
+      {
+        name: "greg",
+        age: 30,
+      },
+      {
+        name: "norm",
+        age: 20,
+      },
+    ],
   });
 });
 
