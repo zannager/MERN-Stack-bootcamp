26748eb401ad67c796a4975dfd8864122efbb1f4
zannager <gzraenga@gmail.com>
2024-07-09 02:42:59 +0800

    React state and event handlers

--- reactbasic/src/App.js
+++ reactbasic/src/App.js
@@ -1,4 +1,13 @@
+import { useState } from "react";
+
 function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("state =>", email, password);
+  };
+
   return (
     <div className='container'>
       <h1 className='display-4 text-center'>Learning react</h1>
@@ -6,17 +15,27 @@ function App() {
 
       <div className='row'>
         <div className='col-md-6 offset-md-r'>
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className='mb-3'>
               <label className='form-label'>Email address</label>
-              <input type='email' className='form-control' />
+              <input
+                value={email}
+                onChange={(event) => setEmail(event.target.value)}
+                type='email'
+                className='form-control'
+              />
               <div className='form-text'>
                 We'll never share your email with anyone else.
               </div>
             </div>
             <div className='mb-3'>
               <label className='form-label'>Password</label>
-              <input type='password' className='form-control' />
+              <input
+                value={password}
+                onChange={(event) => setPassword(event.target.value)}
+                type='password'
+                className='form-control'
+              />
             </div>
             <button type='submit' className='btn btn-primary'>
               Submit
