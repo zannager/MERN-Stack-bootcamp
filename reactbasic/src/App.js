38462c1610351ed6179711f944fde01b5d1fe3b7
zannager <gzraenga@gmail.com>
2024-07-09 13:05:12 +0800

    Rendering data

--- reactbasic/src/App.js
+++ reactbasic/src/App.js
@@ -1,75 +1,35 @@
-//Handling CORS error
+//Rendering data
 
 import { useState } from "react";
 const App = () => {
-  const [user, setUser] = useState("");
+  const [users, setUsers] = useState([]);
   const fechUserData = () => {
-    fetch(`http://localhost:8000/api`, {
+    fetch(`http://localhost:8000/api/users`, {
       method: "GET",
     })
       .then((response) => {
         return response.json();
       })
-      .then((data) => console.log(data))
+      .then((data) => setUsers(data.users))
       .catch((err) => console.log(err));
   };
 
   return (
     <div className='container'>
+      {/* <pre>{JSON.stringify(users, null, 4)}</pre> */}
+
+      {users &&
+        users.map((user) => (
+          <div className='alert alert-primary' key={user.age}>
+            {user.name} age is {user.age} years old
+          </div>
+        ))}
+
       <button onClick={fechUserData} className='btn btn-primary'>
         fetch user data
       </button>
     </div>
   );
 };
-
-// import { useState } from "react";
-
-// function App() {
-//   const [email, setEmail] = useState("");
-//   const [password, setPassword] = useState("");
-//   const handleSubmit = (event) => {
-//     event.preventDefault();
-//     console.log("state =>", email, password);
-//   };
-
-//   return (
-//     <div className='container'>
-//       <h1 className='display-4 text-center'>Learning react</h1>
-//       <br />
-
-//       <div className='row'>
-//         <div className='col-md-6 offset-md-r'>
-//           <form onSubmit={handleSubmit}>
-//             <div className='mb-3'>
-//               <label className='form-label'>Email address</label>
-//               <input
-//                 value={email}
-//                 onChange={(event) => setEmail(event.target.value)}
-//                 type='email'
-//                 className='form-control'
-//               />
-//               <div className='form-text'>
-//                 We'll never share your email with anyone else.
-//               </div>
-//             </div>
-//             <div className='mb-3'>
-//               <label className='form-label'>Password</label>
-//               <input
-//                 value={password}
-//                 onChange={(event) => setPassword(event.target.value)}
-//                 type='password'
-//                 className='form-control'
-//               />
-//             </div>
-//             <button type='submit' className='btn btn-primary'>
-//               Submit
-//             </button>
-//           </form>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
 
 export default App;
