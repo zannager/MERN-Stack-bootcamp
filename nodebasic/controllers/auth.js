13267e86eea9e6f40115d8cf8c78fdf08a0c7703
zannager <gzraenga@gmail.com>
2024-07-16 09:40:35 +0800

    routes and controllers

---
+++ nodebasic/controllers/auth.js
@@ -0,0 +1,15 @@
+exports.users = (req, res) => {
+  //   res.send("hello from node api");
+  res.json({
+    users: [
+      {
+        name: "greg",
+        age: 30,
+      },
+      {
+        name: "norman",
+        age: 20,
+      },
+    ],
+  });
+};
--- nodebasic/index.js
+++ nodebasic/index.js
@@ -3,27 +3,14 @@
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
+const authRoutes = require("./routes/auth");
 
 const app = express();
 
 app.use(cors());
 app.use(morgan("dev"));
 
-//get post put delete
-app.get("/api/users", function (req, res) {
-  //   res.send("hello from node api");
-  res.json({
-    users: [
-      {
-        name: "greg",
-        age: 30,
-      },
-      {
-        name: "norman",
-        age: 20,
-      },
-    ],
-  });
-});
+//routes middleware
+app.use("/api", authRoutes);
 
 app.listen(8000, () => console.log("server is running on port 8000"));
