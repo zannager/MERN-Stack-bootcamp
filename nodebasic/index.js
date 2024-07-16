d790638722042f065c39ffb7c1179619ad2db54b
zannager <gzraenga@gmail.com>
2024-07-16 10:20:15 +0800

    connect to mongodb

--- nodebasic/index.js
+++ nodebasic/index.js
@@ -2,10 +2,25 @@
 
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 const morgan = require("morgan");
 const authRoutes = require("./routes/auth");
 
 const app = express();
+
+//db
+mongoose
+  .connect(
+    "mongodb+srv://projectOne:yGPw803U7b3JJKNw@merncamp.lhd1lgz.mongodb.net/?retryWrites=true&w=majority&appName=merncamp",
+    {
+      useNewUrlParser: true,
+      // useCreateIndex: true, --this is deprecated
+      // useFindAndModify: false, --this is deprecated
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => console.log("DB CONNECTED"))
+  .catch((err) => console.log("DB connection error ", err));
 
 app.use(cors());
 app.use(morgan("dev"));
