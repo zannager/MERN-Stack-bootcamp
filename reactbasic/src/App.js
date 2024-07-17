b3f5c7c05dfd159fe21c8af55dac78ed187babe7
zannager <gzraenga@gmail.com>
2024-07-17 10:25:39 +0800

    react router

--- reactbasic/src/App.js
+++ reactbasic/src/App.js
@@ -1,7 +1,21 @@
 import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Nav from "./components/Nav";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
-  return <Home />;
+  return (
+    <BrowserRouter>
+      <Nav />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
