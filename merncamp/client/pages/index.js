4f8098fdd0214c10ea94b539e67473120fd15883
zannager <gzraenga@gmail.com>
2024-07-18 00:10:39 +0800

    bootstrap css

--- merncamp/client/pages/index.js
+++ merncamp/client/pages/index.js
@@ -3,7 +3,7 @@ const Home = () => {
     <div className='container'>
       <div className='row'>
         <div className='col'>
-          <h1>Home page</h1>
+          <h1 className='display-1 text-center py-5'>Home page</h1>
         </div>
       </div>
     </div>
