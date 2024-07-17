49740b91ef67d7efa53edebb95c39c53777524ec
zannager <gzraenga@gmail.com>
2024-07-18 00:42:32 +0800

    public folder in nextjs

--- merncamp/client/pages/_app.js
+++ merncamp/client/pages/_app.js
@@ -1,9 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Nav from "../components/Nav";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <link rel='stylesheet' href='/css/styles.css' />
+      </Head>
       <Nav />
       <Component {...pageProps} />
     </>
--- merncamp/client/pages/index.js
+++ merncamp/client/pages/index.js
@@ -4,6 +4,7 @@ const Home = () => {
       <div className='row'>
         <div className='col'>
           <h1 className='display-1 text-center py-5'>Home page</h1>
+          <img src='/images/default.jpg' alt='pedestrian' />
         </div>
       </div>
     </div>
