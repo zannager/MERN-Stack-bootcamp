d430360c0a44b53531efd62735ef7048d97d13bd
zannager <gzraenga@gmail.com>
2024-07-18 09:56:57 +0800

    using axios

--- merncamp/client/pages/register.js
+++ merncamp/client/pages/register.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import axios from "axios";
 
 const Register = () => {
   const [name, setName] = useState("");
@@ -8,7 +9,16 @@ const Register = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(name, email, password, secret);
+    // console.log(name, email, password, secret);
+    axios
+      .post("http://localhost:8000/api/register", {
+        name,
+        email,
+        password,
+        secret,
+      })
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
   };
 
   return (
